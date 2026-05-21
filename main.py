d6d49a01d85c494df58bb66be4058ea1a0fcbd99
Shrey Shah <rahulsamariya008@gmail.com>
2026-05-21 16:22:01 +0530

    created the same progarm as the @Raphson still didnt worked

--- main.py
+++ main.py
@@ -2,35 +2,19 @@ from PIL import Image , ImageDraw , ImageFont
 
 import math
 
-import os
-
-from typer import getchar
 # chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
-
-# map pixel brightness (0..255) to a character (chars ordered darkest->lightest)
-# def pixel_to_char(bright):
-#     bright: 0 = black, 255 = white
-#     idx = int((1 - bright / 255) * (len(chars) - 1))  # 0 => darkest char
-#     return chars[idx]
-
-# Example usage inside your loops:
-# h = int((r + g + b) / 3)
-# ch = pixel_to_char(h)
-# ...store/print ch instead of writing grayscale image...
-
 chars = "#Wo- " [::-1]
-
 charArray = list(chars)
 charlength = len(charArray)
 interval = charlength / 256
 
-scaleFactor = 0.3
+scaleFactor = 0.05
 
-onecharwidth = 8
+onecharwidth = 10
 onecharheight = 18
 
-def getchar(input_int):
-    return charArray[math.floor(input_int * interval)]
+def getchar(inputInt):
+    return charArray[math.floor(inputInt*interval)]
 
 text_file = open("output.txt", "w")
 
@@ -39,7 +23,7 @@ im = Image.open("img.jpg")
 fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
 
 width, height = im.size
-im = im.resize((int(scaleFactor * width), int(scaleFactor * height * (onecharwidth / onecharheight))), Image.NEAREST)
+im = im.resize((int(scaleFactor * width), int(scaleFactor*height*(onecharwidth / onecharheight))), Image.NEAREST)
 width, height = im.size
 pix = im.load()
 
@@ -56,7 +40,4 @@ for i in range(height):
 
     text_file.write("\n")
 
-
-im.save("output.png")
-
-os.startfile("output.txt")
+outputImage.save('output.png')
