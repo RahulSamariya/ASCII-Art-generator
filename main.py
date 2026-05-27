d60e6b769f6482375d49086f45242028e52e62f1
Shrey Shah <rahulsamariya008@gmail.com>
2026-05-27 13:02:10 +0530

    improve the brightness issue and added few more samples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image , ImageDraw , ImageFont
+import os 
 
 import math
 
@@ -9,22 +10,24 @@ charArray = list(chars)
 charlength = len(charArray)
 interval = charlength / 256
 
-scaleFactor = 0.3
+scaleFactor = 0.2
 
-onecharwidth = 10
-onecharheight = 18
+onecharwidth = 8
+onecharheight = 16
 
 def getchar(inputInt):
     return charArray[math.floor(inputInt*interval)]
 
 text_file = open("output.txt", "w")
 
-im = Image.open("1.jpg")
+im = Image.open("img.jpg")
+
+brightness = 1.4
 
 fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
 
 width, height = im.size
-im = im.resize((int(scaleFactor * width), int(scaleFactor*height*(onecharwidth / onecharheight))), Image.NEAREST)
+im = im.resize((int(scaleFactor * width), int(scaleFactor*height*(onecharwidth / onecharheight))), Image.BICUBIC)
 width, height = im.size
 pix = im.load()
 
@@ -36,9 +39,19 @@ for i in range(height):
         r, g, b = pix[j, i]
         h = int((r + g + b) / 3)
         pix[j, i] = (h, h, h)
+
         text_file.write(getchar(h))
+        r = int(r * brightness)
+        g = int(g * brightness)
+        b = int(b * brightness)
+
+        r = max(0, min(255, r))
+        g = max(0, min(255, g))
+        b = max(0, min(255, b))
         d.text((j*onecharwidth,  i*onecharheight), getchar(h), font = fnt, fill = (r, g, b))
 
     text_file.write("\n")
 
-outputImage.save('output.png')
+outputImage.save('output3.png')
+
+os.startfile("output3.png")
